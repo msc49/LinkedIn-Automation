47eef242f551c1b835f8892902886acaa6f7403f
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-22 23:29:17 +0000

    created backbone of connections method

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ class LinkedInConnections:
     
     self.find_connections = f"https://www.linkedin.com/search/results/people/?geoUrn=%5B%2290009496%22%5D&keywords={random.choice(self.roles)}&origin=FACETED_SEARCH&position=0&schoolFilter=%5B%2212682%22%5D&searchId=4650e789-7233-4870-a8aa-9068b3f8f3cf&sid=xNy"
     self.driver = webdriver.Chrome()
+
+
+  def connections(self):
+    self.login()
+    self.driver.get(self.find_connections)
+
+    connections = set()
     
 
 
