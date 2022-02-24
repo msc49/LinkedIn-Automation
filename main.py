487ed8bc7d995caa1057b01985fdb8632339c099
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-24 08:26:27 +0000

    url now updates in while loop

--- main.py
+++ main.py
@@ -11,22 +11,24 @@ class LinkedInConnections:
     self.login_page = "https://www.linkedin.com/checkpoint/lg/sign-in-another-account"
     self.roles = ["software-engineer","full-stack-developer", "backend-developer", "front-end-developer", "python-developer", "blockchain-developer"]
     self.page = 1
-    self.find_connections = f"https://www.linkedin.com/search/results/people/?geoUrn=%5B%2290009496%22%5D&keywords={random.choice(self.roles)}&origin=FACETED_SEARCH&page={self.page}&position=0&schoolFilter=%5B%2212682%22%5D&searchId=4650e789-7233-4870-a8aa-9068b3f8f3cf&sid=xNy"
+    self.find_connections = f"https://www.linkedin.com/search/results/people/?geoUrn=%5B%2290009496%22%5D&keywords={random.choice(self.roles)}&origin=FACETED_SEARCH&position=0&schoolFilter=%5B%2212682%22%5D&searchId=4650e789-7233-4870-a8aa-9068b3f8f3cf&sid=xNy"
     self.driver = webdriver.Chrome()
 
 
   def connections(self):
     self.login()
     time.sleep(1.7)
-
+    url = self.find_connections
     
     
     while self.page <= 5:
       time.sleep(1.3)
-      self.driver.get(self.find_connections)
+      self.driver.get(url)
       all_buttons = self.driver.find_elements_by_tag_name("button")
       connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
       self.page += 1
+      url = f"https://www.linkedin.com/search/results/people/?geoUrn=%5B%2290009496%22%5D&keywords={random.choice(self.roles)}&origin=FACETED_SEARCH&page={self.page}&position=0&schoolFilter=%5B%2212682%22%5D&searchId=4650e789-7233-4870-a8aa-9068b3f8f3cf&sid=xNy"
+
 
       for btn in connect_buttons:
         # we will not use btn.click() because although it will work for our first button, the ones after will be intercepted through the blockers linkedin has. Therefore we will click using Javascript
@@ -38,8 +40,9 @@ class LinkedInConnections:
         self.driver.execute_script("arguments[0].click();", close)
         time.sleep(1.7)
 
-      print("now")
-      print(self.page)
+      
+
+      
 
 
 
